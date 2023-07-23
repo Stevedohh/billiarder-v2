f8b58261b5257723b8d352f33ca55d624baa982a
Vladyslav Korotkov <Vladyslav_Korotkov@epam.com>
2023-07-23 20:40:43 +0300

    init

--- apps/client/jest.config.ts
+++ apps/client/jest.config.ts
@@ -1,16 +1,15 @@
 /* eslint-disable */
 export default {
-  displayName: 'billiarder-v2',
+  displayName: 'client',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
-  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/billiarder-v2',
+  coverageDirectory: '../../coverage/apps/client',
 };
---
+++ apps/client/src/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.scss';
+
+function App() {
+  return <div class={styles.App}>Billiarder V2</div>;
+}
+
+export default App;
